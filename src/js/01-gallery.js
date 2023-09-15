59a28860f48262443b092173bfb382b30872c373
Oleksandr Dernoviy <130732150+Aex-Derny@users.noreply.github.com>
2023-09-15 18:25:30 +0300

    jsonEdit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from 'simplelightbox'
+import "simplelightbox/dist/simple-lightbox.min.css"
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+
+import { galleryItems } from './gallery-items'
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems)
+
+const galleryEl = document.querySelector('.gallery')
+galleryEl.insertAdjacentHTML("beforeend", createMarkup(galleryItems))
+    function createMarkup(arr) {
+  return arr
+    .map(
+        ({ preview, original,description}) => `
+    <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      />
+     </a>
+    </li>
+  `
+    )
+    .join("");
+    }
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'top',
+  captionDelay: 250,
+}) 

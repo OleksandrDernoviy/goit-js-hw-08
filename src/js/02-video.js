b0112d46991bbe156a5861c9b2bb6407aebdad34
Oleksandr Dernoviy <130732150+Aex-Derny@users.noreply.github.com>
2023-09-18 12:21:40 +0300

    getItem

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,32 +3,22 @@ import throttle from 'lodash.throttle'
 
 const iframe = document.querySelector('iframe');
 const TIME_KEY = 'videoplayer-current-time';
-// const setTime = localStorage.setItem('TIME_KEY', 'videoplayer-current-time')
 const player = new Player(iframe);
 
-
-// const onPlay = function(data) {
-//     // data is an object containing properties specific to that event
-// };
-// player.on('play', onPlay);
-
-// timeupdate
-// player.setCurrentTime(30.456).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// })
-// jQuery(window).on('scroll', _.throttle(updatePosition, 100));
 
 const onPlay = function (data) {
     localStorage.setItem(TIME_KEY, data.seconds)
 };
 player.on('timeupdate', throttle(onPlay, 1000));
     
-const actualSeconds = JSON.parse(localStorage.getItem(TIME_KEY));
-player.setCurrentTime(actualSeconds);
+let actualSeconds = JSON.parse(localStorage.getItem(TIME_KEY)) || 0;
+console.log(actualSeconds);
+const currentTimePlayer = player.setCurrentTime(actualSeconds);
 
 
-
-
+// if (localStorage.getItem(TIME_KEY) !== null) {
+//  console.log(localStorage.getItem(TIME_KEY));        
+//  
 
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,3 +3,34 @@ const inputEl = document.querySelector('.feedback-form')
 const SAVE_INPUT_KEY = 'feedback-form-state'
 inputEl.addEventListener('input', throttle(inputData, 500))
 inputEl.addEventListener('submit', onSubmit)
+
+let inputForm = JSON.parse(localStorage.getItem(SAVE_INPUT_KEY)) || {};
+
+const { email, message } = inputEl.elements;
+reloadPage();
+
+function inputData(evt) {
+  inputForm = { email: email.value, message: message.value };
+
+  localStorage.setItem(SAVE_INPUT_KEY, JSON.stringify(inputForm));
+}
+
+function reloadPage() {
+  if (inputForm) {
+    email.value = inputForm.email || '';
+    message.value = inputForm.message || '';
+  }
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  
+  if (email.value === '' || message.value === '') {
+    alert('Заповніть будь-ласка всі поля !')
+  };
+
+    localStorage.removeItem(SAVE_INPUT_KEY);
+    evt.currentTarget.reset();
+   inputForm = {};
+}
